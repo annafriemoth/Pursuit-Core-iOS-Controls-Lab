dc9c4a4a17ff16864f5b0b4029b622ad32cc1c6d
Anna Friemoth <annafriemoth@gmail.com>
2019-11-11 16:47:20 -0500

    set up complete

--- ControlsLab/ControlsLab/ViewController.swift
+++ ControlsLab/ControlsLab/ViewController.swift
@@ -2,9 +2,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var selectSuit: UISegmentedControl!
+    
+    @IBOutlet weak var chooseValue: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    var currentSuitSegmentIndex: Int = 0 {
+        didSet {
+            switch selectSuit.selectedSegmentIndex {
+            case 0:
+                if value in selectSuit = 1 {
+                imageView.image =
+                } else if value in selectSuit = 2
+                imageView
+                }
+            case 1:
+                imageView.image =
+            case 2:
+                imageView.image =
+            case 3:
+                imageView.image =
+            default:
+                imageView.image =
+            }
+        }
+    }
+    
+    
+    
+    
 }
 
